6783e30fcf0182e53dc7c62db9698881cde15004
aqibullah <aqibullah3312@gmail.com>
2025-10-17 11:28:57 +0500

    team mode+factory+migration added

---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Team;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class TeamFactory extends Factory
+{
+    protected $model = Team::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'slug' => $this->faker->slug(),
+            'description' => $this->faker->text(),
+            'domain' => $this->faker->word(),
+            'status' => $this->faker->boolean(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+}
