3ad4c5cd2c3a371e16140efc3570e87fd993ccb3
Aqib Ullah <aqibullah3312@gmail.com>
2025-10-23 12:22:57 +0500

    Revert "create new admin panel and moved files from tenant to admin panel"
    This reverts commit 9f07d60dc917260358fbfad9b099251210d8b4de.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\Filament\TenantPanelProvider::class,
+//    App\Providers\StripeBillingProvider::class,
     CodeWithDennis\SimpleAlert\SimpleAlertServiceProvider::class,
 ];
