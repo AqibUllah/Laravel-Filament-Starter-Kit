f8ddfdc1576edca850142ab394b107a1f54541f1
Aqib Ullah <aqibullah3312@gmail.com>
2025-10-27 14:39:17 +0500

    coupon stats added

---
+++ app/Filament/Admin/Widgets/CouponStatsWidget.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Admin\Widgets;
+
+use App\Services\CouponService;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Facades\Filament;
+
+class CouponStatsWidget extends StatsOverviewWidget
+{
+    protected static ?int $sort = 3;
+
+    protected function getStats(): array
+    {
+        $team = Filament::getTenant();
+
+        if (!$team) {
+            return [];
+        }
+
+        $couponService = app(CouponService::class);
+        $stats = $couponService->getCouponUsageStats($team);
+
+        return [
+            Stat::make('Coupons Used', $stats['total_coupons_used'])
+                ->description('Total coupons used by your team')
+                ->descriptionIcon('heroicon-m-tag')
+                ->color('success'),
+
+            Stat::make('Total Savings', '$' . number_format($stats['total_savings'], 2))
+                ->description('Money saved with coupons')
+                ->descriptionIcon('heroicon-m-currency-dollar')
+                ->color('info'),
+
+            Stat::make('Available Coupons', Coupon::active()->forTeam($team->id)->count())
+                ->description('Coupons available for your team')
+                ->descriptionIcon('heroicon-m-gift')
+                ->color('warning'),
+        ];
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Admin\Widgets\CouponStatsWidget;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -39,6 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 AccountWidget::class,
                 FilamentInfoWidget::class,
+                CouponStatsWidget::class
             ])
             ->middleware([
                 EncryptCookies::class,
