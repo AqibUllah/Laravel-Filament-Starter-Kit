16ffc758738d96f15a897868007c80fe1663f5ef
Aqib Ullah <aqibullah3312@gmail.com>
2025-11-06 14:51:55 +0500

    added new provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\EventServiceProvider::class,
+    App\Providers\FeatureLimiterPolicyProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\TenantPanelProvider::class,
     App\Providers\StripeBillingProvider::class,
